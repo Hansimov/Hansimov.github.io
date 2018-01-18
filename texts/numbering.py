7b825f5fa47710ffbd87924246bd857143e97a00
Hansimov <591172499@qq.com>
2018-01-18 23:04:23 +0800

    + Add python doc numbering script

--- texts/numbering.py
+++ texts/numbering.py
@@ -1,14 +1,35 @@
 import re
 
-file_r = open('dx01.csv','r')
+file_r = open('dx01.csv',encoding='utf-8',mode='r')
 file_w = open('dx01_w.txt','w')
 
 start = 0
+mode = 1
+# mode == 1 :
+#   Haven't met the body, discard all lines until a star
+#   Have passed the body, discard all lines after.
+# mode == 2 :
+#   Have met real text and replace star with para number
+#   Other lines will be remained and printed
 
-for line in file.readlines():
-
-
-
+for line in file_r.readlines():
+    if mode == 1:
+        if line.startswith('*'):
+            mode = 2
+            print(start,file=file_w)
+        else:
+            pass
+    elif mode == 2:
+        if line.startswith('*'):
+            start += 1
+            print('\n',start,file=file_w,sep='')
+        elif line.startswith('$$$'):
+            mode = 1
+        else:
+            print(line,file=file_w,end='')
+    else:
+        pass
 
 file_w.close()
 file_r.close()
+
