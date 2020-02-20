87ebdfdd58c6823ac04cf7cf04381cd583ea18ed
Hansimov <591172499@qq.com>
2020-02-20 18:19:27 +0800

    + Add issues

---
+++ backup_scripts/c4d/split_and_delete.py
@@ -0,0 +1,9 @@
+import c4d
+
+def main():
+    c4d.CallCommand(14046, 0) # Split
+    c4d.CallCommand(12109, 0) # Delete
+    doc.SetActiveObject(doc.GetActiveObject().GetNext())
+
+if __name__=='__main__':
+    main()
