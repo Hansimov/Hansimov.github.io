baabceafb62db307bd6df8e0803f7d5f1c803cf6
Hansimov <591172499@qq.com>
2020-04-22 20:21:53 +0800

    + Add sublime settings and some scripts

--- backup_scripts/sharpen_img.py
+++ backup_scripts/sharpen_img.py
@@ -1,24 +1,31 @@
 import os
 from multiprocessing import Pool
 
-root = "./game-engine/"
-outpath = "./game-engine-out/"
-cmd_sharpen = "magick convert -adaptive-sharpen 0x5 \"{}\" \"{}\""
+# root = "./algo-intro/"
+# outpath = "./algo-intro-out/"
+# root = "./os/"
+# outpath = "./os-out/"
+root = "./cpp-stl/"
+outpath = "./cpp-stl-out/"
+# cmd_sharpen = "magick convert -level 48%,100% -contrast-stretch 4% -adaptive-sharpen 0x3 -contrast -contrast \"{}\" \"{}\""
+# cmd_sharpen = "magick convert -level 48%,100% -adaptive-sharpen 0x3 -contrast -contrast \"{}\" \"{}\""
+# cmd_sharpen = "magick convert -level 40%,100% -contrast-stretch 4% -adaptive-sharpen 0x3 -contrast -contrast \"{}\" \"{}\""
+cmd_sharpen = "magick convert -level 40%,100% -adaptive-sharpen 0x3 -contrast -contrast \"{}\" \"{}\""
 
 if not os.path.exists(outpath):
     os.mkdir(outpath)
 
 imgList = os.listdir(root)
 
-def sharpenImg(infilename):
-    print("Processing: " + infilename)
-    name,ext = os.path.splitext(infilename)
-    outfilename = name + "_out" + ext
-    os.system(cmd_sharpen.format(root+infilename,outpath+outfilename))
+def sharpenImg(img_name):
+    print("Processing: " + img_name)
+    name,ext = os.path.splitext(img_name)
+    out_img_name = name + "_out" + ext
+    os.system(cmd_sharpen.format(root+img_name,outpath+out_img_name))
 
 if __name__ == '__main__':
     # pool = Pool(5)
     # pool.map_async(sharpenImg,imgList).get(1)
-    startNum = 500
-    for img in imgList[startNum-1:]:
+    startPage,endPage = 0,-1
+    for img in imgList[startPage:endPage]:
         sharpenImg(img)
