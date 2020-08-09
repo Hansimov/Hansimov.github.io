d98fdd64cfbcbdbb6b3595916304efaac51483c5
asimov <591172499@qq.com>
2020-08-09 13:19:23 +0800

    + Update pdf processing scripts

--- backup_scripts/file-processing/sharpen_img.py
+++ backup_scripts/file-processing/sharpen_img.py
@@ -5,12 +5,13 @@ from multiprocessing import Pool
 # outpath = "./algo-intro-out/"
 # root = "./os/"
 # outpath = "./os-out/"
-root = "./cpp-stl/"
-outpath = "./cpp-stl-out/"
+root = "./破坏之王/"
+outpath = "./破坏之王-out/"
 # cmd_sharpen = "magick convert -level 48%,100% -contrast-stretch 4% -adaptive-sharpen 0x3 -contrast -contrast \"{}\" \"{}\""
 # cmd_sharpen = "magick convert -level 48%,100% -adaptive-sharpen 0x3 -contrast -contrast \"{}\" \"{}\""
 # cmd_sharpen = "magick convert -level 40%,100% -contrast-stretch 4% -adaptive-sharpen 0x3 -contrast -contrast \"{}\" \"{}\""
-cmd_sharpen = "magick convert -level 40%,100% -adaptive-sharpen 0x3 -contrast -contrast \"{}\" \"{}\""
+# cmd_sharpen = "magick convert -level 40%,100% -adaptive-sharpen 0x3 -contrast -contrast \"{}\" \"{}\""
+cmd_sharpen = "magick convert -adaptive-sharpen 0x5 \"{}\" \"{}\""
 
 if not os.path.exists(outpath):
     os.mkdir(outpath)
@@ -26,6 +27,6 @@ def sharpenImg(img_name):
 if __name__ == '__main__':
     # pool = Pool(5)
     # pool.map_async(sharpenImg,imgList).get(1)
-    startPage,endPage = 0,-1
-    for img in imgList[startPage:endPage]:
+    startPage,endPage = 0,10
+    for img in imgList[startPage:]:
         sharpenImg(img)
--- backup_scripts/file-processing/sync_bookmarks.py
+++ backup_scripts/file-processing/sync_bookmarks.py
@@ -4,10 +4,14 @@ t0 = time.time()
 
 mark_str_body = "BookmarkBegin\nBookmarkTitle: {}\nBookmarkLevel: {}\nBookmarkPageNumber: {}\n"
 
-book_fname = "镜头的语法.pdf"
-txt_fname =  "bk-camera-i.txt"
-mark_fname = "bk-camera.txt"
-page_offset = 11
+# book_fname = "镜头的语法.pdf"
+# txt_fname =  "bk-camera-i.txt"
+# mark_fname = "bk-camera.txt"
+book_fname = "破坏之王-DDoS攻击与防范深度剖析.pdf"
+txt_fname =  "bk-phzw-i.txt"
+mark_fname = "bk-phzw.txt"
+
+page_offset = 13 # means page 1 in .txt is page (1+13=)14 in .pdf
 def write_bookmark_from_txt():
     with open(txt_fname,encoding='utf-8', mode = 'r') as rf:
         lines = rf.readlines()
@@ -34,9 +38,9 @@ def write_bookmark_from_txt():
     with open(mark_fname,"w") as wf:
         wf.write(mark_str)
 
-write_bookmark_from_txt()
+# write_bookmark_from_txt()
 name,ext = os.path.splitext(book_fname)
 # os.system("pdftk \"{}\" dump_data output {}".format(book_fname,mark_fname))
-os.system("pdftk \"镜头的语法.pdf\" update_info {} output \"{}\"".format(mark_fname, name+"【书签】"+ext))
+os.system("pdftk \"{}\" update_info {} output \"{}\"".format(book_fname, mark_fname, name+"【书签】"+ext))
 
 print("Elapsed time: {}s".format(round(time.time()-t0,1)))
