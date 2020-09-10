7da9809d04b60ca7ace58129417077452cd2a2b0
asimov <591172499@qq.com>
2020-09-11 01:25:15 +0800

    + Add python file scripts

---
+++ backup_scripts/file-processing/_extract_to_root.py
@@ -0,0 +1,15 @@
+import os
+import shutil
+
+root = "."
+fname_L = os.listdir(root)
+for fname in fname_L:
+    if os.path.isdir(fname):
+        print(fname)
+        for subfname in os.listdir(fname):
+            name, ext = os.path.splitext(subfname)
+            if ext in [".mp4", ".avi", ".mkv"]:
+                old_subfname = os.path.join(root, fname, subfname)
+                new_subfname = os.path.join(root, subfname)
+                print(subfname)
+                shutil.move(old_subfname, new_subfname)
