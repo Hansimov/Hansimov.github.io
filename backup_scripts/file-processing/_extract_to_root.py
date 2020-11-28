1af2caf4823c193b07a05d1de9b24d6246fb09c5
asimov <591172499@qq.com>
2020-11-29 02:37:23 +0800

    + Add some issues

--- backup_scripts/file-processing/_extract_to_root.py
+++ backup_scripts/file-processing/_extract_to_root.py
@@ -1,15 +1,17 @@
 import os
 import shutil
 
-root = "."
+root = "./starname/"
 fname_L = os.listdir(root)
 for fname in fname_L:
-    if os.path.isdir(fname):
-        print(fname)
-        for subfname in os.listdir(fname):
+    if os.path.isdir(root+fname):
+        print(root+fname)
+        for subfname in os.listdir(root+fname):
             name, ext = os.path.splitext(subfname)
-            if ext in [".mp4", ".avi", ".mkv", ".rmvb", ".wmv"]:
+            if ext in [".mp4", ".avi", ".mkv"]:
                 old_subfname = os.path.join(root, fname, subfname)
                 new_subfname = os.path.join(root, subfname)
-                print(subfname)
+                print("\t"+subfname)
+                print("\t\t"+old_subfname)
+                print("\t\t-> "+new_subfname)
                 shutil.move(old_subfname, new_subfname)
