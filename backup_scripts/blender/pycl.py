fbc161dec82144cab3ead05028c24b811303951c
asimov <591172499@qq.com>
2021-05-30 19:28:42 +0800

    + Add blender issue

---
+++ backup_scripts/blender/pycl.py
@@ -0,0 +1,21 @@
+import bpy
+from bpy import context
+
+import builtins as __builtin__
+
+def console_print(*args, **kwargs):
+    for a in context.screen.areas:
+        if a.type == 'CONSOLE':
+            c = {}
+            c['area'] = a
+            c['space_data'] = a.spaces.active
+            c['region'] = a.regions[-1]
+            c['window'] = context.window
+            c['screen'] = context.screen
+            s = " ".join([str(arg) for arg in args])
+            for line in s.split("\n"):
+                bpy.ops.console.scrollback_append(c, text=line)
+
+def print(*args, **kwargs):
+    console_print(*args, **kwargs)       # to Python Console
+    __builtin__.print(*args, **kwargs)   # to System Console
